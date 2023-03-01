f1ac5080dcf0676fb05722f815be15bffd21d9f1
alpanakhirade92 <khiradealpana92@gmail.com>
2023-03-01 23:28:54 +0530

    if loop assignment - 1

---
+++ 06_for_loopAssignment.js
@@ -0,0 +1,46 @@
+console.log(`1. Print no fro 5 th 15 by incrementing by 1..`);
+
+for(i=5 ;i<=15 ; i++){
+    console.log(`${i}`);
+}
+
+console.log(`---------------------------------------------------------`);
+
+console.log(`2. Print no from 50 to 40 by decrementing by 1`);
+
+for(i =50 ; i>=40 ; i--)
+{
+    console.log(`${i}`);
+}
+console.log(`----------------------------------------------------------`);
+console.log(`3. write a prog to find1st 15 odd no`);
+for(var i=1; i<30 ; i=i+2){
+    console.log(`${i}`);
+}
+
+console.log(`----------------------------------------------------------`);
+console.log(`4. write a prog to find 1st 10 even no`);
+for(var i=0; i<=20 ; i=i+2){
+    console.log(`${i}`);
+}
+
+console.log(`----------------------------------------------------------`);
+console.log(`5. write a prog to find table of 5`);
+for(var i=5; i<=50 ; i=i+5){
+    console.log(`${i}`);
+}
+
+console.log(`----------------------------------------------------------`);
+console.log(`6. write a prog to find table of 10`);
+for(var i=10; i<=100 ; i=i+10){
+    console.log(`${i}`);
+}
+
+
+console.log(`----------------------------------------------------------`);
+console.log(`7. write a prog to find reverse from 100 to 10`);
+for(var i=100; i>=10 ; i=i-10){
+    console.log(`${i}`);
+}
+
+
